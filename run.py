08ebdb4d4f5bb27319209d35ee69380756cb32ed
redbasel <jansafar014@gmail.com>
2021-07-16 11:59:04 +0000

    added last wor slice funciton

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -20,7 +21,8 @@ print(data)
 
 def get_sales_data():
     """
-    GET SALES DATA FROM CUSTOMER
+    GET SALES DATA FROM CUSTOMER. runs a whielloop to collct valid string frm user via temrinal.
+     mus t 6  nubmers sperated vy commas. loop wil requiest indefintly until tis valid
 
     """
     while True:
@@ -37,6 +39,8 @@ def get_sales_data():
             print('Data is valid!')
             break
 
+    return sales_data
+
 def validate_data(values):
     """
     Inside the tr, converts all stirng vlae sto integers.
@@ -47,7 +51,7 @@ def validate_data(values):
     try:
         [int(value) for value in values]
         if len(values) != 6:
-            raise ValueError(
+            raise ValueError( 
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
@@ -56,7 +60,41 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+
+    Update sales worksheet, add new row with the list data provided.
+
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print('Sales worksheet updated succesfully.\n')
+
+
+def calculate_surplus_data(sales_row):
+    """
+    compare sales with previus day
+
+    """
+    print("calcaling surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+    
 
 
 
-get_sales_data()
+def main():
+    """
+    Run all program fucntons
+    """
+
+
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+print('welcome ot love sandwiche sdata autoamtion')
+main()
